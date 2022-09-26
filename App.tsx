c159c63d216d43933ce5e71e80c1b7d23001e2af
Andriniaina <andriniainampisoa@gmail.com>
2022-09-26 15:02:44 +0300

    init route and navigation for apps

--- App.tsx
+++ App.tsx
@@ -1,22 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Payeo from './src/payeo.main';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working bien que 
-         on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Payeo/>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
